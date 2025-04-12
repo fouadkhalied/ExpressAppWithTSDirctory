4b29665a297d29c9f94b5d8b0f5e104ca60b8660
fouadkhalied <foukha49@gmail.com>
2025-04-12 21:26:59 +0200

    test database connection

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import { notFoundHandler } from './components/middleware/notFoundHandler';
 import { errorHandler } from './components/middleware/errorHandlert';
-import { connect } from './components/config/db';
+import { connect, pool } from './components/config/db';
 
 const app = express();
 const port = 3000;
@@ -12,8 +12,11 @@ const port = 3000;
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send({ message: 'Hello from Express + TypeScript' });
+app.get('/', async (req: Request, res: Response) => {
+  const obj = await pool.query("SELECT 1 as def")
+  console.log(obj.rows[0].def);
+  
+  res.status(200).send({ message: `Hello from Express + TypeScript + ${obj.rows[0].def}` });
   
 });
 
